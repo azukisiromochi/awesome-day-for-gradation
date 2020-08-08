57e3f368b796aff1a0acf5cdab54aaa0b87e750e
@azukisiromochi <azukisiromochi@gmail.com>
2020-08-08 18:41:44 +0900

    ✨ ヘッダーを追加（コンポーネント化） ⛄

---
+++ src/components/AppHeader.svelte
@@ -0,0 +1,31 @@
+<script>
+  import AppBar from 'smelte/src/components/AppBar';
+  import Button from 'smelte/src/components/Button';
+  import Snackbar from 'smelte/src/components/Snackbar';
+  import { Spacer } from 'smelte/src/components/Util';
+
+  let showSnackbar = false;
+
+  function copyColor() {
+    if(navigator.clipboard){
+      navigator.clipboard.writeText('color');
+      showSnackbar = true;
+    }
+  }
+</script>
+
+<AppBar class="{(i) => i.replace('primary-300', 'dark-transDark')}">
+  <a href="." class="px-2 md:px-8 flex items-center">
+    <img src="/logo-192.png" alt="Smelte logo" width="44" />
+    <h6 class="pl-3 text-white tracking-widest font-thin text-lg">グラデーション日和</h6>
+  </a>
+  <Spacer />
+  <Button color="white" icon="content_copy" text flat on:click="{copyColor}" />
+</AppBar>
+
+<Snackbar top bind:value="{showSnackbar}">
+  <div>Copied the gradation!</div>
+  <div slot="action">
+    <Button text on:click="{() => (showSnackbar = false)}">dummy</Button>
+  </div>
+</Snackbar>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,32 +2,23 @@
   import { stores } from '@sapper/app';
   import { fade } from 'svelte/transition';
 
-  import AppBar from 'smelte/src/components/AppBar';
-  import { Spacer } from 'smelte/src/components/Util';
   import ProgressLinear from 'smelte/src/components/ProgressLinear';
 
+  import AppHeader from '../components/AppHeader.svelte';
   import AppFooter from '../components/AppFooter.svelte';
 
-  const { preloading, page } = stores();
-
-  $: path = $page.path;
+  const { preloading } = stores();
 </script>
 
 <svelte:head>
-  <title>Smelte: Material design using Tailwind CSS for Svelte</title>
+  <title>グラデーション日和</title>
 </svelte:head>
 
 {#if $preloading}
   <ProgressLinear app />
 {/if}
 
-<AppBar class="{(i) => i.replace('primary-300', 'dark-600')}">
-  <a href="." class="px-2 md:px-8 flex items-center">
-    <img src="/logo-192.png" alt="Smelte logo" width="44" />
-    <h6 class="pl-3 text-white tracking-widest font-thin text-lg">グラデーション日和</h6>
-  </a>
-  <Spacer />
-</AppBar>
+<AppHeader />
 
 <main class="container relative p-8 mx-auto h-screen" transition:fade="{{ duration: 300 }}">
   <slot />
