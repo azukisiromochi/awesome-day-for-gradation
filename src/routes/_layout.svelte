88bbb39a10479da5a386ce40329310cde5c03c29
@azukisiromochi <azukisiromochi@gmail.com>
2020-08-25 19:02:48 +0900

    🐎 プレロードを廃止してみる 🐼

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,25 +1,13 @@
 <script>
-  import { stores } from '@sapper/app';
-  import { fade } from 'svelte/transition';
-
-  import ProgressLinear from 'smelte/src/components/ProgressLinear';
-
   import AppMeta from '../components/AppMeta.svelte';
   import AppHeader from '../components/AppHeader.svelte';
   import AppFooter from '../components/AppFooter.svelte';
-
-  const { preloading } = stores();
 </script>
 
 <AppMeta />
-
-{#if $preloading}
-  <ProgressLinear app />
-{/if}
-
 <AppHeader />
 
-<main class="container relative p-8 pt-16 pb-12 mx-auto h-screen" transition:fade="{{ duration: 300 }}">
+<main class="container relative p-8 pt-16 pb-12 mx-auto h-screen">
   <slot />
 </main>
 
